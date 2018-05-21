34b649fcb0a0b61cbaf39c8373567a8d5fe963cb
HemaGRepo <hema.g@capgemini.com>
2018-05-21 15:53:53 +0530

    HelloWorld Code

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+	for(int i=0;i<10;i++)
+        System.out.println( "Welcome to DevOps ...."+i );
+    System.out.println( "Thank U");
+    }
+}
